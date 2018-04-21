8c540b4f8e5fbcb93fc91c310d1064ab6975694e
野本エリカ <ErikaNomoto@nomotoerika-no-macbook-air.local>
2018-04-21 14:56:11 +0900

    Initial Commit

---
+++ Frito１/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frito１
+//
+//  Created by 野本エリカ on 2018/04/21.
+//  Copyright © 2018年 Life is Tech!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
